20eaeeb2c012058e064100b111fc4c8772ea84d1
Hannes Draba <H.Draba@porta.de>
2022-11-09 09:27:31 +0100

    feat logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { postLogin, getLogin } = require('../controllers/auth')
+const { postLogin, getLogin, postLogout } = require('../controllers/auth')
 
 const router = express.Router()
 
 router.get('/login', getLogin)
 router.post('/login', postLogin)
+
+router.post('/logout', postLogout)
 
 module.exports = router
